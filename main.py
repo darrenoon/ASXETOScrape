e471e9313bb62c215a9ec857c5a1284cc365b9b5
darrenoon <45233582+darrenoon@users.noreply.github.com>
2018-12-15 11:08:49 +1000

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@
 # [START gae_python37_app]
 from flask import Flask
 import requests
-from bs4 import BeautifulSoup
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
